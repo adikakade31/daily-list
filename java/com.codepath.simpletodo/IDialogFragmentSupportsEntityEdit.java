3e6e83e838d03271681af86f6e9d73006e6d1f4b
adikakade31 <adi.kakade31@gmail.com>
2016-09-30 20:48:29 -0700

    Create IDialogFragmentSupportsEntityEdit.java

---
+++ java/com.codepath.simpletodo/IDialogFragmentSupportsEntityEdit.java
@@ -0,0 +1,10 @@
+package com.codepath.simpletodo;
+
+import com.codepath.simpletodo.Model.Note;
+
+/**
+ * Created by aditikakadebansal on 9/28/16.
+ */
+public interface IDialogFragmentSupportsEntityEdit {
+    public void setEntityToUpdate(ISimpleTodoEntity entityToUpdate);
+}
