202b390f65aee9dab24874f10b79163aca9fbc6c
adikakade31 <adi.kakade31@gmail.com>
2016-09-30 20:37:30 -0700

    Create CommonConstants.java

---
+++ java/com.codepath.simpletodo/CommonConstants.java
@@ -0,0 +1,19 @@
+package com.codepath.simpletodo;
+
+/**
+ * Created by aditikakadebansal on 9/30/16.
+ */
+public class CommonConstants {
+    public static final String appName = "Forget Me Not";
+    public static final String date = "date";
+    public static final String errorMessage = "Unable to save, Please try again.";
+    public static final String todoValidationMessage = "To-do description is blank.";
+    public static final String yyyyMMddHHmmss = "yyyy-MM-dd HH:mm:ss";
+    public static final String todoSaved = "To-do saved.";
+    public static final String itemText = "itemText";
+    public static final String position = "position";
+    public static final String ToDoTabNAme = "To-Do";
+    public static final String NotesTabNAme = "Notes";
+    public static final String MMddyyyy = "MM-dd-yyyy";
+    public static final String yyyyMMdd = "yyyy-MM-dd";
+}
